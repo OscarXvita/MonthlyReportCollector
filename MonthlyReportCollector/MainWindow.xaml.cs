3dcbe472828b2ce8b27d3eee014923df5ce99929
李丞刚 <oscarxvita@live.com>
2016-10-13 15:43:01 +0800

    封装

--- MonthlyReportCollector/MainWindow.xaml.cs
+++ MonthlyReportCollector/MainWindow.xaml.cs
@@ -176,6 +176,52 @@ namespace MonthlyReportCollector
 
         }
 
+        class MonthlyReport
+        {
+            string iD;
+            public string ID
+            {
+                get { return iD; }
+                set
+                {
+                    if (!Regex.IsMatch(ID, @"[0-9]{4}"))
+                    {
+                        throw new ArgumentException("ID无效或为空！");
+
+                    }
+                    else
+                    {
+                        ID = iD;
+                    }
+                }
+            }
+        
+            string Name;
+            string Sex;
+            string Team;
+            string Phone;
+            string Email;
+            string City;
+            string School;
+            string Major;
+            string Grade;
+            int BlogNum;
+            string BlogLink;
+            int SocialNum;
+            string SocialLink;
+            int Retweets;
+            string RTLink;
+            int PostAccepted;
+            string PostLink;
+            int WindowsApps;
+            string WALink;
+            int ActivityHeldNum;
+            string AHLink;
+            int ActivityJoinNum;
+            string AJNum;
+
+        }
+
         private void btn_SelPath_Click(object sender, RoutedEventArgs e)
         {
 
